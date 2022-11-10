8f3dc5d6c92247b25a4912a636fbf8d2ab2d8051
alimranhossain <alimranhossain44@gmail.com>
2022-11-10 12:42:43 +0600

    service api with id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 
 require("dotenv").config();
@@ -31,12 +31,45 @@ async function run(){
       res.send(services);
     });
      
+    // app.get("/services", async(req, res) => {
+    //   console.log(req.query);
+    //   let query = {};
+    //   const {limit} = req.query;
+    //   if(limit){
+    //     const cursor = serviceCollection.find({}).sort({date: -1}).limit(2);
+    //     // const services = await cursor.toArray();
+    //   }
+    //   else{
+
+    //     const cursor = serviceCollection.find(query);
+    //     res.send(services);
+    //   }
+    //   const services = await cursor.toArray();
+    // });
+
+
+    // app.get("/services/all", async (req, res) => {
+    //    const query = {};
+    //    const cursor = serviceCollection.find(query);
+    //    const servicesAll = await cursor.toArray();
+    //    res.send(servicesAll);
+    //  });
     app.get("/services/all", async (req, res) => {
        const query = {};
        const cursor = serviceCollection.find(query);
-       const servicesAll = await cursor.toArray();
-       res.send(servicesAll);
+       const services = await cursor.toArray();
+       res.send(services);
      });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+
+     
    
   }
 
